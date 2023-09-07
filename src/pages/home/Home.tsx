46dfaef22dad984602a08358ae91db64b9a97df6
Matheus Matos <matheusmatosmatheus@gmail.com>
2023-09-07 17:36:16 -0300

    test

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,10 +3,13 @@ import { Login, Register } from "../../components";
 import { Routes, Route } from "react-router-dom";
 function Home() {
   return (
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <p>teste</p>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
